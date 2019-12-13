a883465d1c95dd0571e72d6a86e81d60a144b474
kamogashira <asuka.kamo@icloud.com>
2019-12-13 22:48:45 +0900

    メインのビュー画面と各ページへのリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'company/index', to: 'company#index'
+  get 'company/about', to: 'company#about'
+  get 'company/flow', to: 'company#flow'
+  get 'company/access', to: 'company#access'
+  root 'company#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
