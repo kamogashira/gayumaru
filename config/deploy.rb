c350f41b43f8949de4d32937458a48fa97c94801
kamogashira <asuka.kamo@icloud.com>
2020-01-10 10:40:56 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'gayumar'
+set :application, 'gayumaru'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:kamogashira/gayumaru.git'
